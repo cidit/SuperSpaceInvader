c47cea911b921a5fe9363df0ab521ecb44382ff4
Cid <ciditworld@gmail.com>
2020-05-01 14:56:41 -0400

    player and weapon behaviour
    both have been made and implemented

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
+    const float MouseSensitivity = 100, MovementSpeed = 10;
+    const float xRange = 20, yRange = 10;
+    WeaponBehaviour weapon;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        WeaponSwitch();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CamControl();
+        MovControl();
+        WeaponSwitch();
+        ShootControl();
+    }
+
+    void CamControl() {
+        var vals = new Vector2(Input.GetAxis("Mouse X"), -Input.GetAxis("Mouse Y"));
+        vals *= Time.deltaTime * MouseSensitivity;
+        transform.Rotate(Vector3.up * vals.x, Space.World);
+        transform.Rotate(Vector3.right * vals.y);
+    }
+
+    void MovControl() {
+        var movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), transform.position.z);
+        movement *= Time.deltaTime * MovementSpeed;
+        var newPosition = movement + transform.position;
+        transform.position = new Vector3(Mathf.Clamp(newPosition.x, -xRange, xRange),
+                                          Mathf.Clamp(newPosition.y, -yRange, yRange));
+
+    }
+
+    void ShootControl() {
+        if (Input.GetKeyDown(KeyCode.Space))
+            weapon.Shoot();
+    }
+
+    void WeaponSwitch() {
+
+        weapon = GetComponentInChildren<WeaponBehaviour>();
+
     }
 }
---
+++ Assets/Scripts/WeaponBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponBehaviour : MonoBehaviour
+{
+
+    const float BulletSpeed = 50000, WeaponRange = 100;
+    Transform BarrelExit;
+
+    [SerializeField] GameObject bulletPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BarrelExit = transform.Find("Barrel Exit");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(transform.position, transform.up, WeaponRange))
+            Debug.Log("In range.");
+    }
+
+    public void Shoot() {
+        var bullet = Instantiate(bulletPrefab, BarrelExit.position, new Quaternion());
+        bullet.GetComponent<Rigidbody>().AddForce(transform.up * Time.deltaTime * BulletSpeed);
+    }
+}
