c2c6accd371bb83c1a062ed3bdd191f8c05f780c
Cid <ciditworld@gmail.com>
2020-05-15 09:15:18 -0400

    second comit
    forgot smth

---
+++ Assets/Scripts/GruntObstacle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class GruntObstacle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = Vector3.forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
